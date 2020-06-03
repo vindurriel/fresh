2f79a8a2e82de16ef03c44373c2d1f8bdceb7fb0
fyang <fyang@freewheel.tv>
2020-06-03 14:22:34 +0800

    sending interrupt instead of kill

--- runner/runner.go
+++ runner/runner.go
@@ -1,53 +1,54 @@
 package runner
 
 import (
-  "flag"
-  "io"
-  "os/exec"
-  "strings"
+	"flag"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 var cmdArgs string
 
 func init() {
-  flag.StringVar(&cmdArgs, "a", "", "Command line arguments to pass to the process")
+	flag.StringVar(&cmdArgs, "a", "", "Command line arguments to pass to the process")
 }
 
 func run() bool {
-  runnerLog("Running...")
+	runnerLog("Running...")
 
-  var cmd *exec.Cmd
-  if cmdArgs != "" {
-    args := strings.Split(cmdArgs, " ")
-    cmd = exec.Command(buildPath(), args...)
-  } else {
-    cmd = exec.Command(buildPath())
-  }
+	var cmd *exec.Cmd
+	if cmdArgs != "" {
+		args := strings.Split(cmdArgs, " ")
+		cmd = exec.Command(buildPath(), args...)
+	} else {
+		cmd = exec.Command(buildPath())
+	}
 
-  stderr, err := cmd.StderrPipe()
-  if err != nil {
-    fatal(err)
-  }
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fatal(err)
+	}
 
-  stdout, err := cmd.StdoutPipe()
-  if err != nil {
-    fatal(err)
-  }
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fatal(err)
+	}
 
-  err = cmd.Start()
-  if err != nil {
-    fatal(err)
-  }
+	err = cmd.Start()
+	if err != nil {
+		fatal(err)
+	}
 
-  go io.Copy(appLogWriter{}, stderr)
-  go io.Copy(appLogWriter{}, stdout)
+	go io.Copy(appLogWriter{}, stderr)
+	go io.Copy(appLogWriter{}, stdout)
 
-  go func() {
-    <-stopChannel
-    pid := cmd.Process.Pid
-    runnerLog("Killing PID %d", pid)
-    cmd.Process.Kill()
-  }()
+	go func() {
+		<-stopChannel
+		pid := cmd.Process.Pid
+		runnerLog("Stopping PID %d", pid)
+		cmd.Process.Signal(os.Interrupt)
+	}()
 
-  return true
+	return true
 }
